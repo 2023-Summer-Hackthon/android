44db92973cbafdc262eb824765a2bbb47aed616b
kimminseong <maga7032@gmail.com>
2023-07-13 02:58:49 +0900

    second

--- app/src/main/java/kr/hs/hackaton2023_1/G_AskActivity.kt
+++ app/src/main/java/kr/hs/hackaton2023_1/G_AskActivity.kt
@@ -1,4 +1,14 @@
 package kr.hs.hackaton2023_1
 
-class G_AskActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kr.hs.hackaton2023_1.databinding.ActivitySignInBinding
+
+class G_AskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_g_ask)
+    }
 }
