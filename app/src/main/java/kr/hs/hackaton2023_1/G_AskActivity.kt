1f6271fde77d08a4153ddf1a0792c42cc66b3e00
kimminseong <maga7032@gmail.com>
2023-07-13 05:53:49 +0900

    forth

--- app/src/main/java/kr/hs/hackaton2023_1/G_AskActivity.kt
+++ app/src/main/java/kr/hs/hackaton2023_1/G_AskActivity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kr.hs.hackaton2023_1.databinding.ActivityLoginBinding
 import kr.hs.hackaton2023_1.databinding.ActivitySignInBinding
 
 class G_AskActivity : AppCompatActivity() {
+    private val binding : ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_g_ask)
+        setContentView(binding.root)
+        val FloatingActionButton : FloatingActionButton = findViewById<FloatingActionButton>(R.id.board_write)
+        FloatingActionButton.setOnClickListener{
+            val intent = Intent(this, )
+            binding.signupBT.setOnClickListener { startActivity(intent) }
+        }
     }
 }
---
+++ app/src/main/java/kr/hs/hackaton2023_1/Post.kt
@@ -0,0 +1,4 @@
+package kr.hs.hackaton2023_1
+
+class Post {
+}
